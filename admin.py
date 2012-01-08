0578a440a2b87b59d4f4a5b3163ec792c21b7dae
tioover <tioover@gmail.com>
2012-01-08 19:18:09 +0800

    fix bug

--- admin.py
+++ admin.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python2
 # coding=utf-8
-import sys
 import os
 import readline
 import elixir
@@ -8,20 +7,13 @@ import getpass
 
 from application import orm
 
-def router():
-    orm.metadata.bind.echo = True
-    elixir.setup_all(True)
-    option = sys.argv[1:]
-    if (not option) or option[0] == 'help': admin_help()
-    elif option[0] == 'init': init()
-    elif option[0] == 'user':
-        if option[1] == 'new': new_user()
-
 def init():
     if os.path.exists('database'):
         os.remove('database') #remove sqlite datebase
-    if raw_input('Are you create new user?') == 'y':
-        new_user()
+    orm.metadata.bind.echo = True
+    elixir.setup_all(True)
+    raw_input('Are you create new user?')
+    new_user()
 
 def new_user():
     print('New user')
@@ -31,10 +23,5 @@ def new_user():
     orm.Person(name, email, password)
     orm.session.commit()
 
-def admin_help():
-    print('init              : Initialize the database')
-    print('user              : User Management')
-    print('user new          : Create_user')
 
-
-if __name__ == '__main__': router()
+if __name__ == '__main__': init()
