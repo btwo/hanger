7c0985e4b1dea4b1655567dc62c8663d9d7597cd
tioover <tioover@gmail.com>
2012-02-07 18:46:52 +0800

    给脚本加上执行权限

--- hanger.py
+++ hanger.py
@@ -1,16 +1,30 @@
 #!/usr/bin/env python2
 # coding=utf-8
 import sys
+import os
+import shutil
+
 from optparse import OptionParser
 
-parser = OptionParser()
-parser.add_option('-n', '--new', action='store', dest='project_name',
-    help='Create new project.')
-parser.add_option('-v', '--version', action='store_true', dest='version',
-    default=False, help='Show the version of this command.')
+def new_proj(projname, projpath):
+    path = os.path.split(os.path.realpath(__file__))[0] + '/hanger'
+    projpath = projpath + '/' + projname
+    shutil.copytree(path, projpath)
 
-(options, args) = parser.parse_args(sys.argv)
-if options.project_name:
-    print options.project_name
-elif options.version:
-    print '12.2.7'
+def main():
+    parser = OptionParser()
+    parser.add_option('-n', '--new', action='store', dest='project_name',
+        help='Create new project.')
+    parser.add_option('-t', '--to', action='store', dest='project_path',
+        help='New project path.')
+    parser.add_option('-v', '--version', action='store_true', dest='version',
+        default=False, help='Show the version of this command.')
+
+    (options, args) = parser.parse_args(sys.argv)
+    if options.project_name and options.project_path:
+        new_proj(options.project_name, options.project_path)
+    elif options.version:
+        print '12.2.7'
+
+if __name__ == '__main__':
+    main()
