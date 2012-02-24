c3717b99f1839b5d740b0a7f2da7dae760909e4c
tioover <tioover@gmail.com>
2012-02-24 22:24:29 +0800

    modified base

--- hanger/lib/base.py
+++ hanger/lib/base.py
@@ -8,45 +8,54 @@ from tornado import web
 from model import getitem, Person
 from jinja2 import Environment, FileSystemLoader
 
-class Base(web.RequestHandler):
-    '''Base Handler'''
-    templname = ''
+class BaseHandler(web.RequestHandler):
+    def __init__(self, *args):
+        super(BaseHandler, self).__init__(*args)
+        name = str(self.__class__.__name__) # Class name.
+        self.name = name
+
+
+class FormHandler(BaseHandler):
     forms = {}
 
     def __init__(self, *args):
-        super(Base, self).__init__(*args)
-        name = str(self.__class__.__name__) # Class name.
-        self.name = name
-        # default template.
-        self.templname = name + '.html'
+        super(FormHandler, self).__init__(*args)
         # default form.
         try:
-            self.forms[name] = eval('forms.' + name)
+            self.forms[self.name] = eval('forms.' + self.name)
         except AttributeError:
             pass
 
-    def json_write(self, obj):
-        self.set_header('Content-Type', 'application/json')
-        self.write(json.dumps(obj))
+    def form_loader(self, key = None):
+        '''Return instance of form.'''
+        if not key:
+            key = self.name
+        form = self.forms[self.name]
+        return form(self)
 
-    def redirect(self, path = None):
-        if not path:
-            path = self.get_argument("next", None)
-            if not path:
-                path = '/'
-        super(Base, self).redirect(path)
+    def form_add(self, form_name):
+        '''Add form to self.forms. '''
+        self.forms[form_name] = eval('forms.' + form_name)
 
-    def get_current_user(self):
-        cookie = self.get_secure_cookie('user')
-        if not cookie:
+    def form_validate(self, form, key = None, **kwargs):
+        '''Automated handle of Forms validate.'''
+        if not key:
+            key = self.name # default form key.
+        if not form.validate():
+            self.render({key: form}, **kwargs)
             return False
-        user_json = json.loads(cookie)
-        user = getitem(Person, user_json['id'], show_error = False)
-        if not user:
-            return False
-        return user
+        else:
+            return True
 
-    # These is templates handle methods.
+
+class TemplateHandler(FormHandler):
+    templname = ''
+
+    def __init__(self, *args):
+        super(TemplateHandler, self).__init__(*args)
+        # default template.
+        self.templname = self.name + '.html'
+
     def render(self, formobj_dict = None, **kwargs):
         # Auto load form.
         formdict = {}
@@ -54,8 +63,18 @@ class Base(web.RequestHandler):
             formdict[key] = self.forms[key]() # Instance of form.
         if formobj_dict:
             formdict.update(formobj_dict) # merger.
-        super(Base, self).render(self.templname, forms = formdict, **kwargs)
+        super(TemplateHandler, self).render(self.templname, forms = formdict,
+            **kwargs)
 
+    #def get_error_html(self, status_code, **kwargs):
+    #    if status_code == 404:
+    #        return self.render_string('errors/404.html', **kwargs)
+    #    else:
+    #        self.write(str(status_code) + ' error')
+    #    return
+
+
+class JinjaHandler(TemplateHandler):
     def render_string(self, template_name, **kwargs):
         '''Template render by Jinja2.'''
         default = {
@@ -84,31 +103,25 @@ class Base(web.RequestHandler):
         template = env.get_template(template_name)
         return template
 
-    #def get_error_html(self, status_code, **kwargs):
-    #    if status_code == 404:
-    #        return self.render_string('errors/404.html', **kwargs)
-    #    else:
-    #        self.write(str(status_code) + ' error')
-    #    return
 
-    # These is form handle methods.
-    def form_loader(self, key = None):
-        '''Return instance of form.'''
-        if not key:
-            key = self.name
-        form = self.forms[self.name]
-        return form(self)
+class Base(JinjaHandler):
+    def json_write(self, obj):
+        self.set_header('Content-Type', 'application/json')
+        self.write(json.dumps(obj))
 
-    def form_add(self, form_name):
-        '''Add form to self.forms. '''
-        self.forms[form_name] = eval('forms.' + form_name)
+    def redirect(self, path = None):
+        if not path:
+            path = self.get_argument("next", None)
+            if not path:
+                path = '/'
+        super(Base, self).redirect(path)
 
-    def form_validate(self, form, key = None, **kwargs):
-        '''Automated handle of Forms validate.'''
-        if not key:
-            key = self.name # default form key.
-        if not form.validate():
-            self.render({key: form}, **kwargs)
+    def get_current_user(self):
+        cookie = self.get_secure_cookie('user')
+        if not cookie:
             return False
-        else:
-            return True
+        user_json = json.loads(cookie)
+        user = getitem(Person, user_json['id'], show_error = False)
+        if not user:
+            return False
+        return user
