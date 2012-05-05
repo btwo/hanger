bc37f4039221201998299d7d44236bd2a54cf569
tioover <tioover@gmail.com>
2012-05-05 21:43:55 +0800

    style and html

--- app/__init__.py
+++ app/__init__.py
@@ -11,13 +11,14 @@ from config import settings
 from view import Home, SignIn, SignUp, SignOut, PersonPage, Settings
 from base import Base
 
-class Error404(Base):
+class PageNotFound(Base):
     '''If url not belonging to any handler, raise 404error.'''
     def get(self):
         raise web.HTTPError(404)
 
     def post(self):
         raise web.HTTPError(404)
+
 
 routes = [
     # This is url route rule
@@ -27,7 +28,7 @@ routes = [
     (r'/signout/?', SignOut),
     (r'/person/(\d+)/?', PersonPage),
     (r'/settings/?', Settings),
-    (r'.*', Error404), # Place to end.
+    (r'.*', PageNotFound), # Place to end.
 ]
 
 
