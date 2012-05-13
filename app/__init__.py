38ea414ecfe572d54f0807c09b29caaa25224f65
tioover <tioover@gmail.com>
2012-05-13 15:53:28 +0800

    change templates env

--- app/__init__.py
+++ app/__init__.py
@@ -5,16 +5,8 @@ PATH = utils.realpath(__file__)
 from tornado import web
 from config import settings
 from lib.database import SQLAlchemy
-from jinja2 import Environment, FileSystemLoader
 
 db = SQLAlchemy('sqlite:////tmp/hanger.db') # first run, run db.create_all().
-
-jinja_env = Environment(
-    # load template in file system.
-    loader = FileSystemLoader(settings['template_path']),
-    auto_reload = settings['debug'], #auto reload
-    autoescape = False, # auto escape
-)
 
 from view import Home, SignIn, SignUp, SignOut, PersonPage, Settings,\
         PageNotFound
@@ -33,6 +25,6 @@ routes = [
 class Application(web.Application):
     def __init__(self):
         super(Application, self).__init__(
-            routes, jinja_env=jinja_env, **settings)
+            routes, **settings)
 
 application = Application()
