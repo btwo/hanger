16c114911ecae102d7932e53e71a1ff246767608
tioover <tioover@gmail.com>
2012-06-01 22:01:32 +0800

    nginx config.

--- example/config.py
+++ example/config.py
@@ -5,15 +5,15 @@ from example import PATH, ui
 
 settings = dict(
     # general
-    site_name = u'hanger',
-    site_domain = 'eggshell.me',
+    site_name = u'YourSiteName',
+    site_domain = 'your.url',
     debug = True,
     ui_methods = ui,
     port = 8888,
     login_url = '/signin/',
     # Security
     xsrf_cookies = True,
-    cookie_secret = r'哈哈哈你看看你',
+    cookie_secret = r'哈哈你看看你!!',
     # path
     template_path = join(PATH, 'templates'),
     static_path = join(PATH, 'static'),
@@ -22,6 +22,6 @@ settings = dict(
     send_error_mail = True,
     admin_mail = 'tioover@gmail.com',
     mail_host = '127.1',
-    mail_postfix = 'eggshell.me' # site domain.
 )
+settings['mail_postfix'] = settings['site_domain']
 settings['avatar_path'] = join(settings['static_path'], 'avatar')
