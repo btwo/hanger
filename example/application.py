e2d44deb94056d6ca4ad9a990c8fe95818a74ef5
tioover <tioover@gmail.com>
2012-06-09 21:06:56 +0800

    add Application support media and handlers make

--- example/application.py
+++ example/application.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2.7
 # coding=utf-8
-from tornado import web
+from hanger import Hanger
 from hanger.database import Elixir
 from redis import StrictRedis
 from config import config
@@ -11,22 +11,15 @@ db = Elixir(config['database_url']) # first run, run db.create_all().
 import user
 import views
 
-class Application(web.Application):
+class Application(Hanger):
     def __init__(self):
         self.redis = StrictRedis(host='localhost', port=config['redis_port'],
             db=config['redis_db'])
-        # Settings extend.
         #url roues
-        routes = []
-        modules = [user, views]
-        for view in modules:
-            routes.extend(view.routes)
-        routes.extend([
-            (r"/media/(.*)", web.StaticFileHandler, {
-                "path": config['media_path']}),
+        end_handlers = [
             ('.*', views.PageNotFound), # 404 page, place bottom.
-        ])
-
+        ]
+        routes = self.route(user, views, end_handlers=end_handlers)
         super(Application, self).__init__(routes, **config)
 
 application = Application()
--- hanger/__init__.py
+++ hanger/__init__.py
@@ -12,6 +12,25 @@ import utils
 import forms
 
 
+class Hanger(web.Application):
+    def __init__(self, handlers=None, *args, **settings):
+        media_handler = (r"/media/(.*)", web.StaticFileHandler, {
+            "path": settings['media_path']})
+        if settings.has_key("media_path"):
+            handlers.insert(0, media_handler)
+        super(Hanger, self).__init__(handlers, *args, **settings)
+
+    def route(self, *modules, **addhandlers):
+        routes = []
+        if addhandlers.has_key("top"):
+            routes.extend(addhandlers['top'])
+        for module in modules:
+            if hasattr(module, "routes"):
+                routes.extend(module.routes)
+        if addhandlers.has_key("end_handlers"):
+            routes.extend(addhandlers['end_handlers'])
+        return routes
+
 class BaseHandler(web.RequestHandler):
     '''
     Base handler is parent class of all handlers.
